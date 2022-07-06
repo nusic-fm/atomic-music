ce73c18a51956aa31805e07b103656f21335d3ee
zeeshanhanif <zeeshanhanif@gmail.com>
2022-07-06 11:42:34 +0500

    Added script for list child and find root owner

---
+++ scripts/06_find-root-owner.ts
@@ -0,0 +1,27 @@
+import { BigNumber, ContractReceipt, ContractTransaction } from 'ethers';
+import { ethers } from 'hardhat';
+import { AtomicMusicNFT, AtomicMusicNFT__factory } from '../typechain';
+/*
+* Main deployment script to deploy all the relevent contracts
+*/
+async function main() {
+  const [owner, add1] = await ethers.getSigners();
+
+  const AtomicMusicNFT:AtomicMusicNFT__factory = await ethers.getContractFactory("AtomicMusicNFT");
+  const atomicMusicNFT:AtomicMusicNFT = await AtomicMusicNFT.attach("0xC57d6963f184Bff298d38b47276C0ECe1199f76c");
+  
+  console.log("AtomicMusicNFT Address:", atomicMusicNFT.address);
+  let tokenId = 2;
+  const rootOwnerAddress = await atomicMusicNFT.findRootOwner(tokenId);
+  console.log(`Root Owner of TokenId ${tokenId} is: ${rootOwnerAddress}`);
+
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
