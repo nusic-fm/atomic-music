2e845430f4eb0df3417f3b4819ae1cc06636d3b2
zeeshanhanif <zeeshanhanif@gmail.com>
2022-07-11 11:24:03 +0500

    Added ERC20 Mock for USDC

---
+++ contracts/mock/USDCMock.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * Main ERC20 Token for Panacloud which will be used for Platform Governance DAO
+ */
+contract USDCMock is ERC20, Ownable {
+
+    constructor() ERC20("Mock USDC", "MUSDC") {
+        // 10k initial supply
+        _mint(msg.sender, 10_000 * 10 ** decimals());
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return 6;
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
