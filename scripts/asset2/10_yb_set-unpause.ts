9c4e1a7a4175cea2513996ff895704f6f7f16416
zeeshanhanif <zeeshanhanif@gmail.com>
2022-07-13 01:00:37 +0500

    Added scripts for pause and unpause contract mint/transfer

---
+++ scripts/asset2/10_yb_set-unpause.ts
@@ -0,0 +1,29 @@
+import { BigNumber, ContractReceipt, ContractTransaction } from 'ethers';
+import { ethers } from 'hardhat';
+import { AtomicMusicYBNFT, AtomicMusicYBNFT__factory } from '../../typechain';
+const addresses = require("./address.json");
+/*
+* Main deployment script to deploy all the relevent contracts
+*/
+async function main() {
+  const [owner, add1] = await ethers.getSigners();
+
+  const AtomicMusicYBNFT:AtomicMusicYBNFT__factory = await ethers.getContractFactory("AtomicMusicYBNFT");
+  const atomicMusicYBNFT:AtomicMusicYBNFT = await AtomicMusicYBNFT.attach(addresses.nftAddress);
+
+  const txt = await atomicMusicYBNFT.unpause();
+  console.log("unpause contract txt.hash = ",txt.hash);
+  const txtReceipt = await txt.wait();
+  console.log("unpause contract txt.hash = ",txtReceipt);
+
+}
+
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
---
+++ scripts/asset2/11_yb_get-pause-status.ts
@@ -0,0 +1,28 @@
+import { BigNumber, ContractReceipt, ContractTransaction } from 'ethers';
+import { ethers } from 'hardhat';
+import { AtomicMusicYBNFT, AtomicMusicYBNFT__factory } from '../../typechain';
+const addresses = require("./address.json");
+/*
+* Main deployment script to deploy all the relevent contracts
+*/
+
+async function main() {
+  const [owner, add1] = await ethers.getSigners();
+
+  const AtomicMusicYBNFT:AtomicMusicYBNFT__factory = await ethers.getContractFactory("AtomicMusicYBNFT");
+  const atomicMusicYBNFT:AtomicMusicYBNFT = await AtomicMusicYBNFT.attach(addresses.nftAddress);
+
+  const isPaused = await atomicMusicYBNFT.paused()
+  console.log("isPaused = ",isPaused);
+
+}
+
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
