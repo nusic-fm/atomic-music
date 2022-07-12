bd5b6d24f2bbe96a45f2b84951b98db57a83b8df
zeeshanhanif <zeeshanhanif@gmail.com>
2022-07-12 19:47:45 +0500

    Added feature to have separate price for each token

--- contracts/AtomicMusicNFT.sol
+++ contracts/AtomicMusicNFT.sol
@@ -16,20 +16,7 @@ contract AtomicMusicNFT is ERC721Pausable, Ownable {
         uint256 tokenId;
         uint256 parentTokenId;
         bool isMinted;
-    }
-
-    struct Child {
-        address contractAddress;
-        uint256 tokenId;
-        address baseAddr;
-        bytes8 equipSlot;
-        bool pending;
-    }
-
-    struct RoyaltyData {
-        address royaltyAddress;
-        uint32 numerator;
-        uint32 denominator;
+        uint256 price;
     }
 
     mapping(uint256 => TokenInfo[]) public _childrenMetadata;
@@ -44,12 +31,7 @@ contract AtomicMusicNFT is ERC721Pausable, Ownable {
     address public admin;
     address public manager;
 
-    uint256 public minPrice = 50e15; // 0.05
-    uint256 public maxPrice = 100e15; // 0.1
-
-    constructor(string memory _name, string memory _symbol, uint256 _minPrice, uint256 _maxPrice, string memory _defaultURI) ERC721(_name,_symbol){
-        minPrice = _minPrice;
-        maxPrice = _maxPrice;
+    constructor(string memory _name, string memory _symbol, string memory _defaultURI) ERC721(_name,_symbol){
         defaultURI = _defaultURI;
         baseURI = _defaultURI;
     }
@@ -72,12 +54,13 @@ contract AtomicMusicNFT is ERC721Pausable, Ownable {
         return children;
     }
 
-    function addChildMetadata(uint256 rootTokenId, TokenInfo[] memory _childrens) public onlyOwner {
+    function addChildMetadata(uint256 rootTokenId, uint256 rootTokenPrice, TokenInfo[] memory _childrens) public onlyOwner {
         _rootTokens[rootTokenId] = true;
         _rootTokenInfo[rootTokenId] = TokenInfo({
             tokenId:rootTokenId,
             parentTokenId: 0,
-            isMinted: false
+            isMinted: false,
+            price: rootTokenPrice
         });
         totalTokens++;
         for (uint256 i = 0; i < _childrens.length; i++) {
@@ -91,10 +74,12 @@ contract AtomicMusicNFT is ERC721Pausable, Ownable {
     }
 
     function mintRoot(uint256 tokenId) public payable {
-        require(!_exists(tokenId), "ERC721: token already minted");
+        require(tokenId != 0, "Not mintable");
+        require(!_exists(tokenId), "Token already minted");
         require(_rootTokens[tokenId], "Token Id is not for Root token");
         require(!isChildMinted(tokenId), "Child has already been minted for this token");
-        require(msg.value >= minPrice && msg.value <= maxPrice, "Insufficient Funds Sent" );
+        //require(msg.value >= minPrice && msg.value <= maxPrice, "Insufficient Funds Sent" );
+        require(msg.value == _rootTokenInfo[tokenId].price, "Insufficient Funds Sent");
         _safeMint(msg.sender, tokenId);
         if(_rootTokens[tokenId]) {
             _rootTokenInfo[tokenId].isMinted = true;
@@ -103,13 +88,14 @@ contract AtomicMusicNFT is ERC721Pausable, Ownable {
     }
 
     function mint(uint256 tokenId, uint256 parentTokenId) public payable {
-        require(!_exists(tokenId), "ERC721: token already minted");
+        require(!_exists(tokenId), "Token already minted");
         require(!_exists(parentTokenId), "Parent token already minted");
         require(!isParentMinted(parentTokenId), "Parent has already been minted for this token");
-        require(msg.value >= minPrice && msg.value <= maxPrice, "Insufficient Funds Sent" );
+        //require(msg.value >= minPrice && msg.value <= maxPrice, "Insufficient Funds Sent" );
 
         for (uint256 i = 0; i < _childrenMetadata[parentTokenId].length; i++) {
             if(_childrenMetadata[parentTokenId][i].tokenId == tokenId) {
+                require(msg.value == _childrenMetadata[parentTokenId][i].price, "Insufficient Funds Sent");
                 _safeMint(msg.sender, tokenId);
                 _childrenMetadata[parentTokenId][i].isMinted = true;
                 totalMinted++;
@@ -165,16 +151,34 @@ contract AtomicMusicNFT is ERC721Pausable, Ownable {
         require(sent, "Failed to withdraw Ether");
     }
 
-    function setMinPrice(uint256 _minPrice) public onlyOwner {
-        minPrice = _minPrice;
-    }
-
-    function setMaxPrice(uint256 _maxPrice) public onlyOwner {
-        maxPrice = _maxPrice;
-    }
-
     function tokenExists(uint256 tokenId) public view returns (bool) {
         return _exists(tokenId);
     }
     
+    function setPrice(uint256 tokenId, uint256 parentTokenId, uint256 price) public onlyOwner{
+        bool found = false;
+        for (uint256 i = 0; i < _childrenMetadata[parentTokenId].length; i++) {
+            if(_childrenMetadata[parentTokenId][i].tokenId == tokenId) {
+                require(!_childrenMetadata[parentTokenId][i].isMinted, "Token already minted");
+                _childrenMetadata[parentTokenId][i].price = price;
+                found = true;
+            }
+        }
+        require(found, "Parent and Child token id mismatch");
+    }
+
+    function setRootPrice(uint256 rootTokenId, uint256 price) public onlyOwner{
+        require(!_rootTokenInfo[rootTokenId].isMinted, "Token already minted");
+        require(_rootTokens[rootTokenId], "Not a Root tokens");
+        _rootTokenInfo[rootTokenId].price = price;
+    }
+
+    function getPrice(uint256 tokenId, uint256 parentTokenId) public view returns (uint256) {
+        for (uint256 i = 0; i < _childrenMetadata[parentTokenId].length; i++) {
+            if(_childrenMetadata[parentTokenId][i].tokenId == tokenId) {
+                return _childrenMetadata[parentTokenId][i].price;
+            }
+        }
+        revert("Parent and Child token id mismatch");
+    }    
 }
