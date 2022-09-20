8d3a1749930b4281791654193b36564f50ab332c
zeeshanhanif <zeeshanhanif@gmail.com>
2022-09-20 23:46:20 +0500

    Added script to pause contract

---
+++ scripts/asset3/08_set-pause.ts
@@ -0,0 +1,29 @@
+import { ethers } from 'hardhat';
+import { AtomicMusicMCNFT, AtomicMusicMCNFT__factory } from '../../typechain';
+const addresses = require("./address.json");
+/*
+* Main deployment script to deploy all the relevent contracts
+*/
+
+async function main() {
+  const [owner, add1] = await ethers.getSigners();
+
+  const AtomicMusicMCNFT:AtomicMusicMCNFT__factory = await ethers.getContractFactory("AtomicMusicMCNFT");
+  const atomicMusicMCNFT:AtomicMusicMCNFT = await AtomicMusicMCNFT.attach(addresses.nftAddress);
+
+  const txt = await atomicMusicMCNFT.pause();
+  console.log("pause contract txt.hash = ",txt.hash);
+  const txtReceipt = await txt.wait();
+  console.log("pause contract txt.hash = ",txtReceipt);
+
+}
+
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
