5b38a1694235dc13c5b245e415903664941ee72c
zeeshanhanif <zeeshanhanif@gmail.com>
2022-11-12 00:43:55 +0500

    Added NFTMock and WETH Mock

---
+++ contracts/mock/WETHMock.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * Main ERC20 Token for Panacloud which will be used for Platform Governance DAO
+ */
+contract WETHMock is ERC20, Ownable {
+
+    constructor() ERC20("Mock WETH", "MWETH") {
+        // 10k initial supply
+        _mint(msg.sender, 10_000_000 * 10 ** decimals());
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return 18;
+    }
+
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
+}
