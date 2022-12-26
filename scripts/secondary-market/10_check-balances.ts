0a9ebceffadb23b1cee04ab2937b74f75ec4575f
zeeshanhanif <zeeshanhanif@gmail.com>
2022-12-26 23:31:57 +0500

    Added script to check balances

---
+++ scripts/secondary-market/10_check-balances.ts
@@ -0,0 +1,31 @@
+import { ethers } from 'hardhat';
+import { MasterContract, MasterContract__factory } from '../../typechain';
+const addresses = require("./address.json");
+/*
+* Main deployment script to deploy all the relevent contracts
+*/
+
+async function main() {
+  const [owner, addr1, addr2, addr3] = await ethers.getSigners();
+
+  const MasterContract:MasterContract__factory =  await ethers.getContractFactory("MasterContract");
+  const masterContract:MasterContract = await MasterContract.attach(addresses.masterContractAddress);
+  console.log("MasterContract Address to:", masterContract.address);
+
+  const marketPlaceBalance = await masterContract.marketPlaceBalance();
+  console.log("marketPlaceBalance = ",marketPlaceBalance.toString());
+
+  const addr2UserBalance = await masterContract.userBalance(addr2.address);
+  console.log("addr2UserBalance = ",addr2UserBalance.toString());
+  
+}
+
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
